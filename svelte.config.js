5ea6fcc785c232f512ae5cfe7effb0fc7b205341
kodaoluidris <kodaoluidris@gmail.com>
2024-11-21 09:43:56 +0100

    fixed deployment issue

--- svelte.config.js
+++ svelte.config.js
@@ -3,14 +3,18 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		paths: {
+			base: process.env.BASE_PATH || ''
+		},
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
 			fallback: '404.html',
+			precompress: false,
 			strict: true
 		}),
-		paths: {
-			base: process.env.BASE_PATH || ''
+		prerender: {
+			entries: ['*']
 		}
 	}
 };
